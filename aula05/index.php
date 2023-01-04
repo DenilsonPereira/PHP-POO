ed3093a1128d2c43cc492dc35223c4dc0065e81b
DenilsonPereira <denilsongunner@gmail.com>
2023-01-04 10:57:17 -0300

    Update index.php

--- aula05/index.php
+++ aula05/index.php
@@ -4,6 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        *{
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        pre{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 400px;
+        }
+    </style>
     <title>Banco Denilson</title>
 </head>
 <body>
